95240abbdb3691f083804b36e451ee773f45bb9d
thk7964 <yjs7894@gmail.com>
2025-12-18 16:21:07 +0900

    Lv 1-2: JWT의 이해

--- src/main/java/org/example/expert/config/JwtUtil.java
+++ src/main/java/org/example/expert/config/JwtUtil.java
@@ -25,6 +25,7 @@ public class JwtUtil {
 
     @Value("${jwt.secret.key}")
     private String secretKey;
+
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
@@ -34,7 +35,7 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(bytes);
     }
 
-    public String createToken(Long userId, String email, UserRole userRole) {
+    public String createToken(Long userId, String email, UserRole userRole,String nickname ) {
         Date date = new Date();
 
         return BEARER_PREFIX +
@@ -42,6 +43,7 @@ public class JwtUtil {
                         .setSubject(String.valueOf(userId))
                         .claim("email", email)
                         .claim("userRole", userRole)
+                        .claim("nickname ", nickname)
                         .setExpiration(new Date(date.getTime() + TOKEN_TIME))
                         .setIssuedAt(date) // 발급일
                         .signWith(key, signatureAlgorithm) // 암호화 알고리즘
